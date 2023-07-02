10944fcb44b730aa948e7339e1f3d4291ba3eda6
artemnefedov <to.artemnefedov@gmail.com>
2023-07-02 21:15:29 +0400

    update JavaAITest.java

--- README.md
+++ README.md
@@ -6,72 +6,108 @@
 ## JavaAI is an open source library that allows you to interact with OpenAI models with just a few lines of code
 
 #### Note:
-> I wrote this library for myself since I couldn't find anything more suitable for my project, so I decided to share it with the community. I hope this library will help you.<br>
-Any help is welcome. I am always glad to hear new ideas and criticism.
+
+> I wrote this library for myself since I couldn't find anything more suitable for my project, so I decided to share it
+> with the community. I hope this library will help you.<br>
+> Any help is welcome. I am always glad to hear new ideas and criticism.
 
 ## How to use it?
 
 ### Import the library
 
 #### Maven:
+
 ```xml
+
 <dependency>
     <groupId>io.github.artemnefedov</groupId>
     <artifactId>javaai</artifactId>
-    <version>0.3.3</version>
+    <version>0.3.4</version>
 </dependency>
 ```
+
 #### Gradle:
+
 ```groovy
-implementation group: 'io.github.artemnefedov', name: 'javaai', version: '0.3.3'
+implementation group: 'io.github.artemnefedov', name: 'javaai', version: '0.3.4'
 ```
 
-### Initialize OpenAI
+### Initialize JavaAI
+
 ```java
-OpenAI openAI = new OpenAIImplementation("YOUR_OPEN_AI_API-KEY");
+JavaAI javaAI=javaAiBuilder("YOUR_OPEN_AI_API-KEY");
 ```
-### Set the query parameters and get the result from the OpenAI model
 
+## Java AI example
+
+### ChatGPT
+
+>You have 2 options to use JavaAI to work with ChatGPT.
+You can make a first request using List<ChatMessage> to set the context and get a response, and after that use a string.
+<br>Both options retain the message history.
+<br>The "**assistant**" role is used by default for answers, be careful.
+
+```java
+var messages=List.of(
+        new ChatMessage("user","Hello!"),
+        new ChatMessage("assistant","Hello! How can I assist you today?"));
+
+String chatResponse = javaAI.chat(messages);
+```
+#### _OR_
+```java
+javaAI.chat("What's 2 2?"); // answer: 2 + 2 equals 4.
+javaAI.chat("What did I ask in the last question?"); //answer: In your last question, you asked "What's 2 2?"
+```
+
+### Completions
 ```java
 //Text generation, the model will return the response as a String
-String response = openAI.generateText("Say this is a test"))
-
-//Image generation, the model will return a URL/ to the result, as a List of String
-List<String> imges = openAI.generateImage("A cute baby sea otter"));
-
-//Text generation, taking into account the dialogue (GPT-3.5), the model will return the answer as a String
-List<ChatMessage> messages = new ArrayList<>();
-
-messages.add(new ChatMessage("user", "Hello!"));
-
-String chatResponse = openAI.chat(messages);
+String response = javaAI.generateText("Say this is a test");
 ```
-#### Just in case, I added a [demo](https://github.com/artemnefedov/JavaAI/tree/demo)
-<br>
+
+### DALL·E 2
+```java
+//Image generation, the model will return a URL/ to the result, as a List of String
+String imgUrl=javaAI.generateImage("A cute baby sea otter");
+```
+
 ---
 
 **Notice:**
-<br>
+
 > You can always set your parameters for the models
 
-Example for ImageBuilder:
+Example for Chat:
+
 ```java
-ImageBuilder imageBuilder = new ImageBuilder( 1, "1024x1024", "url");
-openAI.customImageBuilderConfig(imageBuilder);
+javaAI.setChat(
+        Chat.builder()
+            .messages(new ArrayList<>())
+            .model("gpt-3.5-turbo")
+            .maxTokens(2000)
+            .n(1)
+            .build()
+        );
 ```
 
 ---
+
 ## Models that JavaAI works with:
+
 1. [x] [Completions](https://platform.openai.com/docs/api-reference/completions)
 2. [x] [Chat-GPT](https://platform.openai.com/docs/api-reference/chat)
 3. [x] [Create image](https://platform.openai.com/docs/api-reference/images/create)
 
 ---
+
 ## Outside Dependencies.
-#### This library uses:
-* [Gson](https://github.com/google/gson), to convert JSON to Java objects.<br>
-* [lombok](https://github.com/projectlombok/lombok) to make the code clearer.<br>
-* [junit](https://github.com/junit-team/junit5) for testing.
+
+* [Gson](https://github.com/google/gson)
+* [lombok](https://github.com/projectlombok/lombok)
+* [junit](https://github.com/junit-team/junit5)
+* [logback-classic](https://mvnrepository.com/artifact/ch.qos.logback/logback-classic)
 
 ## License
+
 #### Distributed under the [MIT License](https://github.com/artemnefedov/JavaAI/blob/main/LICENSE)
--- src/main/java/io/github/artemnefedov/javaai/service/JavaAI.java
+++ src/main/java/io/github/artemnefedov/javaai/service/JavaAI.java
@@ -1,96 +1,96 @@
-/*
- *  MIT License
- *
- *  Copyright (c)  2023. Artyom Nefedov
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- */
-
-package io.github.artemnefedov.javaai.service;
-
-import io.github.artemnefedov.javaai.dto.ChatMessage;
-import io.github.artemnefedov.javaai.dto.Chat;
-import io.github.artemnefedov.javaai.dto.Completions;
-
-import java.util.List;
-
-/**
- * The main class for interacting with JavaAI implements the {@link JavaAI} interface.
- */
-public interface JavaAI {
-
-    /**
-     * The method that generates the text interacts with the
-     * <a href="https://platform.openai.com/docs/api-reference/completions">Completions</a>.
-     *
-     * @param prompt parameters for text generation.
-     * @return the response from the API as a string
-     */
-    String generateText(String prompt);
-
-    /**
-     * The method that generates the image interacts with the
-     * <a href="https://platform.openai.com/docs/api-reference/images/create">Create image</a>.
-     *
-     * @param prompt parameters for image generation.
-     * @return the response from the API as a string(url)
-     */
-    String generateImage(String prompt);
-
-    /**
-     * The method that generates text, taking into account chat messages,
-     * uses a <a href="https://platform.openai.com/docs/api-reference/chat">Chat</a>.
-     *
-     * @param messages List of {@link ChatMessage}  containing your chat by role.
-     * @return the response from the API as a string
-     */
-    String chat(List<ChatMessage> messages);
-
-    /**
-     * The method that generates text, taking into account chat messages,
-     * uses a <a href="https://platform.openai.com/docs/api-reference/chat">Chat</a>.
-     *
-     * @param userMessage your message with the user role.
-     * @return the response from the API as a string
-     */
-    String chat(String userMessage);
-
-    /**
-     * Sets your dto to work with the API
-     * @param completions dto with your options.
-     */
-    void setCompletions(Completions completions);
-
-    /**
-     * Sets your dto to work with the API
-     *
-     * @param chat dto with your options.
-     */
-    void setChat(Chat chat);
-
-    /**
-     * Sets default options for dto, usually this should be enough to get the job done.
-     */
-    void defaultCompetitionsConfig();
-
-    /**
-     * Sets default options for dto, usually this should be enough to get the job done.
-     */
-    void defaultChatConfig();
-}
+///*
+// *  MIT License
+// *
+// *  Copyright (c)  2023. Artyom Nefedov
+// *
+// *  Permission is hereby granted, free of charge, to any person obtaining a copy
+// *  of this software and associated documentation files (the "Software"), to deal
+// *  in the Software without restriction, including without limitation the rights
+// *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// *  copies of the Software, and to permit persons to whom the Software is
+// *  furnished to do so, subject to the following conditions:
+// *
+// *  The above copyright notice and this permission notice shall be included in all
+// *  copies or substantial portions of the Software.
+// *
+// *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// *  SOFTWARE.
+// */
+//
+//package io.github.artemnefedov.javaai.service;
+//
+//import io.github.artemnefedov.javaai.dto.ChatMessage;
+//import io.github.artemnefedov.javaai.dto.Chat;
+//import io.github.artemnefedov.javaai.dto.Completions;
+//
+//import java.util.List;
+//
+///**
+// * The main class for interacting with JavaAI implements the {@link JavaAI} interface.
+// */
+//public interface JavaAI {
+//
+//    /**
+//     * The method that generates the text interacts with the
+//     * <a href="https://platform.openai.com/docs/api-reference/completions">Completions</a>.
+//     *
+//     * @param prompt parameters for text generation.
+//     * @return the response from the API as a string
+//     */
+//    String generateText(String prompt);
+//
+//    /**
+//     * The method that generates the image interacts with the
+//     * <a href="https://platform.openai.com/docs/api-reference/images/create">Create image</a>.
+//     *
+//     * @param prompt parameters for image generation.
+//     * @return the response from the API as a string(url)
+//     */
+//    String generateImage(String prompt);
+//
+//    /**
+//     * The method that generates text, taking into account chat messages,
+//     * uses a <a href="https://platform.openai.com/docs/api-reference/chat">Chat</a>.
+//     *
+//     * @param messages List of {@link ChatMessage}  containing your chat by role.
+//     * @return the response from the API as a string
+//     */
+//    String chat(List<ChatMessage> messages);
+//
+//    /**
+//     * The method that generates text, taking into account chat messages,
+//     * uses a <a href="https://platform.openai.com/docs/api-reference/chat">Chat</a>.
+//     *
+//     * @param userMessage your message with the user role.
+//     * @return the response from the API as a string
+//     */
+//    String chat(String userMessage);
+//
+//    /**
+//     * Sets your dto to work with the API
+//     * @param completions dto with your options.
+//     */
+//    void setCompletions(Completions completions);
+//
+//    /**
+//     * Sets your dto to work with the API
+//     *
+//     * @param chat dto with your options.
+//     */
+//    void setChat(Chat chat);
+//
+//    /**
+//     * Sets default options for dto, usually this should be enough to get the job done.
+//     */
+//    void defaultCompetitionsConfig();
+//
+//    /**
+//     * Sets default options for dto, usually this should be enough to get the job done.
+//     */
+//    void defaultChatConfig();
+//}
