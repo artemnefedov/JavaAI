8e1ac35f7e64e50beb8e3750fd7f204be7666e7c
Artem Nefedov <artemnefedov@duck.com>
2024-09-27 11:20:34 +0400

    added tests for main service
    Signed-off-by: Artem Nefedov <artemnefedov@duck.com>

--- src/test/java/io/github/artemnefedov/javaai/JavaAITest.java
+++ src/test/java/io/github/artemnefedov/javaai/JavaAITest.java
@@ -1,16 +1,11 @@
 package io.github.artemnefedov.javaai;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-
 import io.github.artemnefedov.javaai.model.chat.ChatConfig;
 import io.github.artemnefedov.javaai.model.chat.ChatMessage;
 import io.github.artemnefedov.javaai.model.dalle.DalleConfig;
 import io.github.artemnefedov.javaai.model.tts.TtsConfig;
 import io.github.artemnefedov.javaai.service.Connection;
 import io.github.artemnefedov.javaai.service.impl.JavaAIImpl;
-import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +13,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class JavaAITest {
@@ -42,8 +44,7 @@ class JavaAITest {
         JSONArray jsonArray = new JSONArray();
         jsonArray.put(expectedUrlObject);
 
-        Mockito.when(connectionMock.getJsonResponse(any()))
-                .thenReturn(new JSONObject().put("data", jsonArray));
+        when(connectionMock.getJsonResponse(any())).thenReturn(new JSONObject().put("data", jsonArray));
 
         String result = javaAI.generateImage("test prompt");
         assertEquals(expectedUrl, result);
@@ -54,8 +55,8 @@ class JavaAITest {
         List<String> expectedUrls = List.of("URL 1", "URL 2");
         JSONArray jsonArray = new JSONArray();
         expectedUrls.forEach(url -> jsonArray.put(new JSONObject().put("url", url)));
-        Mockito.when(connectionMock.getJsonResponse(any()))
-                .thenReturn(new JSONObject().put("data", jsonArray));
+
+        when(connectionMock.getJsonResponse(any())).thenReturn(new JSONObject().put("data", jsonArray));
 
         javaAI.setDalleConfig(new DalleConfig(
                 DalleConfig.DalleModel.DALL_E_2,
@@ -73,7 +74,8 @@ class JavaAITest {
     @Test
     void textToSpeech_CreatesFileSuccessfully() {
         byte[] audioResponse = new byte[]{0, 1, 2, 3};
-        Mockito.when(connectionMock.getAudioResponse(any())).thenReturn(audioResponse);
+
+        when(connectionMock.getAudioResponse(any())).thenReturn(audioResponse);
 
         boolean result = javaAI.textToSpeech("test prompt", "target/", "testFile");
         assertTrue(result);
@@ -83,12 +85,12 @@ class JavaAITest {
     void chat_ReturnsExpectedResponse() {
         String expectedResponse = "Test response";
 
-        JSONArray jsonArray = new JSONArray()
-                .put(new JSONObject().put("message",
-                        new JSONObject().put("content", "Test response")).put("index", 0));
+        JSONArray jsonArray = new JSONArray().put(new JSONObject().put(
+                "message",
+                new JSONObject().put("content", "Test response")
+        ).put("index", 0));
 
-        Mockito.when(connectionMock.getJsonResponse(any()))
-                .thenReturn(new JSONObject().put("choices", jsonArray));
+        when(connectionMock.getJsonResponse(any())).thenReturn(new JSONObject().put("choices", jsonArray));
 
         String result = javaAI.chat(List.of(new ChatMessage("user", "Hello")));
         assertEquals(expectedResponse, result);
@@ -97,19 +99,20 @@ class JavaAITest {
     @Test
     void chatWithChoices_ReturnsExpectedResponses() {
 
-        List<String> expectedMessages = List.of("Test response 1", "Test response 2",
-                "Test response 3");
+        List<String> expectedMessages = List.of("Test response 1", "Test response 2", "Test response 3");
 
         JSONArray jsonArray = new JSONArray()
-                .put(new JSONObject().put("message",
-                        new JSONObject().put("content", "Test response 1")).put("index", 0))
-                .put(new JSONObject().put("message",
-                        new JSONObject().put("content", "Test response 2")).put("index", 1))
-                .put(new JSONObject().put("message",
-                        new JSONObject().put("content", "Test response 3")).put("index", 2));
+                .put(new JSONObject()
+                             .put("message", new JSONObject().put("content", "Test response 1"))
+                             .put("index", 0))
+                .put(new JSONObject()
+                             .put("message", new JSONObject().put("content", "Test response 2"))
+                             .put("index", 1))
+                .put(new JSONObject()
+                             .put("message", new JSONObject().put("content", "Test response 3"))
+                             .put("index", 2));
 
-        Mockito.when(connectionMock.getJsonResponse(any()))
-                .thenReturn(new JSONObject().put("choices", jsonArray));
+        when(connectionMock.getJsonResponse(any())).thenReturn(new JSONObject().put("choices", jsonArray));
 
         javaAI.setChatConfig(new ChatConfig(
                 ChatConfig.Model.GPT_4,
@@ -131,33 +134,53 @@ class JavaAITest {
 
     @Test
     void setChatConfig_AppliesConfigCorrectly() {
-        ChatConfig chatConfig = new ChatConfig(ChatConfig.Model.GPT_4, 0.5f, 1, 5, false, null, 100,
-                0.1f, 0.1f, null, "user");
+        ChatConfig chatConfig = new ChatConfig(
+                ChatConfig.Model.GPT_4,
+                0.5f,
+                1,
+                5,
+                false,
+                null,
+                100,
+                0.1f,
+                0.1f,
+                null,
+                "user"
+        );
         javaAI.setChatConfig(chatConfig);
-        // Проверка, что конфигурация была применена, может включать проверку значений полей в объекте chat
+
         assertEquals(0.5f, javaAI.getChat().getConfig().temperature());
         assertEquals(5, javaAI.getChat().getConfig().n());
     }
 
     @Test
     void setDalleConfig_AppliesConfigCorrectly() {
-        DalleConfig dalleConfig = new DalleConfig(DalleConfig.DalleModel.DALL_E_2, 3, "high",
-                DalleConfig.ResponseFormat.URL, DalleConfig.Size.D1_512x512,
-                DalleConfig.Style.VIVID, "user");
+        DalleConfig dalleConfig = new DalleConfig(
+                DalleConfig.DalleModel.DALL_E_2,
+                3,
+                "high",
+                DalleConfig.ResponseFormat.URL,
+                DalleConfig.Size.D1_512x512,
+                DalleConfig.Style.VIVID,
+                "user"
+        );
         javaAI.setDalleConfig(dalleConfig);
-        // Проверка, что конфигурация была применена
+
         assertEquals(3, javaAI.getDalle().getConfig().n());
         assertEquals("high", javaAI.getDalle().getConfig().quality());
     }
 
     @Test
     void setTtsConfig_AppliesConfigCorrectly() {
-        TtsConfig ttsConfig = new TtsConfig(TtsConfig.TtsModel.TTS_1, TtsConfig.Voice.ALLOY,
-                TtsConfig.VoiceResponseFormat.MP3, 1.0f);
+        TtsConfig ttsConfig = new TtsConfig(
+                TtsConfig.TtsModel.TTS_1,
+                TtsConfig.Voice.ALLOY,
+                TtsConfig.VoiceResponseFormat.MP3,
+                1.0f
+        );
         javaAI.setTtsConfig(ttsConfig);
-        // Проверка, что конфигурация была применена
+
         assertEquals(TtsConfig.Voice.ALLOY, javaAI.getTts().getConfig().voice());
-        assertEquals(TtsConfig.VoiceResponseFormat.MP3,
-                javaAI.getTts().getConfig().responseFormat());
+        assertEquals(TtsConfig.VoiceResponseFormat.MP3, javaAI.getTts().getConfig().responseFormat());
     }
 }
